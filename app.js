251e9dc2a29e6132522d6816f058ffc649057d94
Remilekun Odegbami <remdev22@gmail.com>
2022-04-27 04:53:00 +0100

    created a pixel art maker using Javascript

---
+++ app.js
@@ -0,0 +1,48 @@
+const sizePicker = document.querySelector(".size-picker");
+const inputHeight = document.querySelector("#inputHeight");
+const inputWidth = document.querySelector("#inputWidth");
+
+const color = document.querySelector("#colorPicker");
+
+const pixelCanvas = document.querySelector("#pixelCanvas")
+
+
+// function to create grid
+function makeGrid(e) {
+    e.preventDefault();
+
+    // clear grid everytime button is clicked
+    clearGrid();
+
+    const height = inputHeight.value;
+    const width = inputWidth.value;
+
+    for (let i = 0; i <= height; i++) {
+        // create table row
+        const row = document.createElement("tr");
+        for (let j = 1; j <= width; j++) {
+            // create table column
+            const column = document.createElement("td");
+            column.id = "column-i-j"
+            // append column to row
+            row.appendChild(column);
+        }
+        //append row to pixelCanvas
+        pixelCanvas.appendChild(row);
+    }
+}
+
+// clear grid function
+function clearGrid() {
+    const gridRows = document.querySelectorAll("tr");
+    gridRows.forEach((row) => {
+      row.remove();
+    });
+}
+
+// When size is submitted by the user, call makeGrid()
+sizePicker.addEventListener("submit", makeGrid);
+
+pixelCanvas.addEventListener("click", function (e) {
+  e.target.style.backgroundColor = color.value;
+});
